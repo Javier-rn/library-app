ed8e7c1227b08900a6801b15eddd3a1d1fdfce30
Javier <javier96rn@gmail.com>
2022-05-19 12:09:44 +0200

    Books can now be created and added to the webpage

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const button = document.querySelector('button');
 const form = document.querySelector('.form');
 const addBtn = document.querySelector('#add-btn');
 
+const container = document.querySelector('.container');
+
+const titleInput = document.getElementById('input-title');
+const authorInput = document.getElementById('input-author');
+const pagesInput = document.getElementById('input-nPages');
+
 button.addEventListener('click', function () {
   form.classList.toggle('invisible');
 });
@@ -15,8 +21,79 @@ function Book(title, author, nPages, read) {
   this.read = read;
 }
 
+// const book1 = new Book('Game of Thrones', 'George RR Martin', 800, true);
+
 function addBookToLibrary(book) {
-  myLibrary.push(book);
+  // WORKING
 }
 
-const book1 = new Book('Game Of Thrones', 'George RR Martin', 800, true);
+addBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const readStatus = document.querySelector('input[name="read-status"]:checked').value;
+
+  const newBook = new Book(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    readStatus
+  );
+  myLibrary.push(newBook);
+
+  createCards(myLibrary);
+});
+
+function createCards(booksArr) {
+  container.innerHTML = '';
+  booksArr.forEach((book) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const cardTop = document.createElement('div');
+    cardTop.classList.add('card-top');
+
+    const titleP = document.createElement('p');
+    titleP.setAttribute('id', 'title');
+    titleP.textContent = book.title;
+
+    const authorP = document.createElement('p');
+    authorP.setAttribute('id', 'author');
+    authorP.textContent = book.author;
+
+    const cardBottom = document.createElement('div');
+    cardBottom.classList.add('card-bottom');
+
+    const pagesP = document.createElement('p');
+    pagesP.setAttribute('id', 'pages');
+    pagesP.textContent = book.nPages;
+
+    const readP = document.createElement('p');
+    readP.setAttribute('id', 'read-status');
+
+    book.read === true ? (readP.textContent = 'Read') : (readP.textContent = 'Not read');
+
+    cardTop.appendChild(titleP);
+    cardTop.appendChild(authorP);
+
+    cardBottom.appendChild(pagesP);
+    cardBottom.appendChild(readP);
+
+    card.appendChild(cardTop);
+    card.appendChild(cardBottom);
+
+    container.appendChild(card);
+  });
+}
+
+{
+  /*   <div class="card">
+        <div class="card-top">
+          <p id="title">Game of Thrones</p>
+          <p id="author">George RR Martin</p>
+        </div>
+        <div class="card-bottom">
+          <p>800 pages</p>
+          <p>Read</p>
+        </div>
+      </div> */
+}
